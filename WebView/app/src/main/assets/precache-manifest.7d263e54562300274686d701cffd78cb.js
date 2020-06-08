6d149934c2e86172990cc215af99aafe3033bf5c
Amir Karamuja <amirkaramuja@gmail.com>
2020-06-08 16:35:59 -0400

    Fix location requests

---
+++ WebView/app/src/main/assets/precache-manifest.7d263e54562300274686d701cffd78cb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab9e8e15b954382908ab7606ed9439c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0df1c96e52e7f0b3a0f5",
+    "url": "./static/css/2.09ce8bc7.chunk.css"
+  },
+  {
+    "revision": "56b534095a59aba95980",
+    "url": "./static/css/main.63b5996c.chunk.css"
+  },
+  {
+    "revision": "0df1c96e52e7f0b3a0f5",
+    "url": "./static/js/2.a2fe3a38.chunk.js"
+  },
+  {
+    "revision": "8d26b2e3700774aaa3e681d097f59854",
+    "url": "./static/js/2.a2fe3a38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b534095a59aba95980",
+    "url": "./static/js/main.e711725f.chunk.js"
+  },
+  {
+    "revision": "9d44efefe000c89335a2",
+    "url": "./static/js/runtime-main.75110d1e.js"
+  },
+  {
+    "revision": "99110196af1e8b68856d4c2684ea7512",
+    "url": "./static/media/entry.99110196.jpg"
+  },
+  {
+    "revision": "6a59cdc84d4f6c3e28961509a8526f35",
+    "url": "./static/media/house.6a59cdc8.jpg"
+  },
+  {
+    "revision": "26d686dd4768e0fd820cecd8c3aa1e76",
+    "url": "./static/media/livingroom.26d686dd.jpg"
+  },
+  {
+    "revision": "3d73387c2a1e21f207a8357724f536ba",
+    "url": "./static/media/owfont-regular.3d73387c.ttf"
+  },
+  {
+    "revision": "e00c269cd4fb37b24eea2e4f37982c3d",
+    "url": "./static/media/owfont-regular.e00c269c.eot"
+  },
+  {
+    "revision": "ed668bf0b123371be46d005b428f6188",
+    "url": "./static/media/owfont-regular.ed668bf0.woff"
+  },
+  {
+    "revision": "f3ce0ef136eaaf2b1db22cf4069cb5dc",
+    "url": "./static/media/owfont-regular.f3ce0ef1.svg"
+  }
+]);
